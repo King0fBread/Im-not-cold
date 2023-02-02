77f5e5b968b88cd499286aca3b9d813fdf7a8ae7
Nikita Glubokov <ITVenomancer@gmail.com>
2023-02-02 19:27:14 +0300

    Added basic negative states displayer

---
+++ I'm not cold/Assets/Scripts/NegativeStatesDisplayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NegativeStatesDisplayer : MonoBehaviour
+{
+    [Header("Sliders")]
+    [SerializeField] private Slider _heatSlider;
+    [SerializeField] private Slider _energySlider;
+    [SerializeField] private Slider _hungerSlider;
+    [SerializeField] private Slider _mentalSlider;
+    [Header("States icons")]
+    [SerializeField] private GameObject _heatShortageState;
+    [SerializeField] private GameObject _energyShortageState;
+    [SerializeField] private GameObject _hungerShortageState;
+    [SerializeField] private GameObject _mentalShortageState;
+
+    private void Update()
+    {
+        ObserveSliderAndActivateState(_heatSlider, _heatShortageState);
+        ObserveSliderAndActivateState(_energySlider, _energyShortageState);
+        ObserveSliderAndActivateState(_hungerSlider, _hungerShortageState);
+        ObserveSliderAndActivateState(_mentalSlider, _mentalShortageState);
+    }
+    private void ObserveSliderAndActivateState(Slider slider, GameObject stateIcon)
+    {
+        if (slider.value <= 0 && !stateIcon.activeSelf)
+            stateIcon.SetActive(true);
+        else if (slider.value > 0 && stateIcon.activeSelf)
+            stateIcon.SetActive(false);
+    }
+}
