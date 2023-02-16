48df0191d47060fa4fbfda646310b1e4c2a8c3fe
Nikita Glubokov <ITVenomancer@gmail.com>
2023-02-16 08:20:23 +0300

    Added the sound manager

---
+++ I'm not cold/Assets/Scripts/SoundsManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundsManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer _masterMixer;
+    private AudioClip _clip;
+    private AudioSource _source;
+    public AudioClipAndSourcePairedToSound[] audioClipsAndSourcePairedToSounds;
+    [System.Serializable]
+    public class AudioClipAndSourcePairedToSound
+    {
+        public Sounds sound;
+        public AudioClip clip;
+        public AudioSource source;
+    }
+
+    private SoundsManager _instance;
+    public SoundsManager instance { get { return _instance; } }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    public enum Sounds
+    {
+        PlayerJumpingSnow,
+        PlayerJumpingWood,
+        PlayerPickUpItem,
+        PlayerRunningWood,
+        PlayerWalkingSnow,
+        PlayerWalkingSoft,
+        PlayerWalkingWood,
+    }
+    private void PlaySound(Sounds sound)
+    {
+        GetRequestedAudioClipAndAudioSource(sound, out _clip, out _source);
+        if (!_source.isPlaying)
+        {
+            _source.PlayOneShot(_clip);
+        }
+    }
+    private void StopSound(Sounds sound)
+    {
+        GetRequestedAudioClipAndAudioSource(sound, out _clip, out _source);
+        if (_source.isPlaying)
+        {
+            _source.Stop();
+        }
+    }
+    private void GetRequestedAudioClipAndAudioSource(Sounds soundToFind, out AudioClip clip, out AudioSource source)
+    {
+        clip = null;
+        source = null;
+
+        foreach (AudioClipAndSourcePairedToSound audioClipAndSourcePairedToSound in audioClipsAndSourcePairedToSounds)
+        {
+            if (audioClipAndSourcePairedToSound.sound == soundToFind)
+            {
+                clip = audioClipAndSourcePairedToSound.clip;
+                source = audioClipAndSourcePairedToSound.source;
+            }
+        }
+    }
+}
