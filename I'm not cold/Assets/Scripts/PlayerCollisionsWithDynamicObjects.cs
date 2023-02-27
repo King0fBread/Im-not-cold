fae30e3e09ea37854a90f5521163cd4920759413
Nikita Glubokov <ITVenomancer@gmail.com>
2023-02-27 08:38:41 +0300

    Replaced wood and metal sounds

--- I'm not cold/Assets/Scripts/PlayerCollisionsWithDynamicObjects.cs
+++ I'm not cold/Assets/Scripts/PlayerCollisionsWithDynamicObjects.cs
@@ -8,10 +8,12 @@ public class PlayerCollisionsWithDynamicObjects : MonoBehaviour
     {
         if (collision.gameObject.GetComponent<WoodenObject>())
         {
+            SoundsManager.instance.StopSound(SoundsManager.Sounds.PlayerTouchedWood);
             SoundsManager.instance.PlaySound(SoundsManager.Sounds.PlayerTouchedWood);
         }
         else if (collision.gameObject.GetComponent<MetallicObject>())
         {
+            SoundsManager.instance.StopSound(SoundsManager.Sounds.PlayerTouchedMetal);
             SoundsManager.instance.PlaySound(SoundsManager.Sounds.PlayerTouchedMetal);
         }
     }
